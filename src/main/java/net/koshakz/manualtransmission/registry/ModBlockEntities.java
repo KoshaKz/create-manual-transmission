7042d4291db2aba096494e771ba3ad39f4fc7218
KoshaKz <viskas228228@gmail.com>
2026-02-05 12:12:12 +0100

    Register SteeringWheelRenderer in ModBlockEntities

--- src/main/java/net/koshakz/manualtransmission/registry/ModBlockEntities.java
+++ src/main/java/net/koshakz/manualtransmission/registry/ModBlockEntities.java
@@ -3,12 +3,14 @@ package net.koshakz.manualtransmission.registry;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import net.koshakz.manualtransmission.ManualTransmission;
 import net.koshakz.manualtransmission.content.steering.SteeringWheelBlockEntity;
+import net.koshakz.manualtransmission.content.steering.SteeringWheelRenderer;
 
 public class ModBlockEntities {
     
     public static final BlockEntityEntry<SteeringWheelBlockEntity> STEERING_WHEEL = ManualTransmission.REGISTRATE
             .blockEntity("steering_wheel", SteeringWheelBlockEntity::new)
             .validBlocks(ModBlocks.STEERING_WHEEL)
+            .renderer(() -> SteeringWheelRenderer::new)
             .register();
 
     public static void register() {}
