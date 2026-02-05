27de62a824b032c941c2b81376d8a483c89df047
KoshaKz <viskas228228@gmail.com>
2026-02-05 12:00:01 +0100

    Update ManualTransmission.java to register blocks and entities

--- src/main/java/net/koshakz/manualtransmission/ManualTransmission.java
+++ src/main/java/net/koshakz/manualtransmission/ManualTransmission.java
@@ -1,6 +1,11 @@
 package net.koshakz.manualtransmission;
 
+import com.tterrag.registrate.Registrate;
+import net.koshakz.manualtransmission.registry.ModBlockEntities;
+import net.koshakz.manualtransmission.registry.ModBlocks;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +14,16 @@ public class ManualTransmission {
     public static final String MOD_ID = "manualtransmission";
     public static final String NAME = "Create: Manual Transmission";
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+    public static final Registrate REGISTRATE = Registrate.create(MOD_ID);
 
     public ManualTransmission() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        ModBlocks.register();
+        ModBlockEntities.register();
+        
+        REGISTRATE.registerEventListeners(modEventBus);
+        
         LOGGER.info("Manual Transmission mod initialized!");
-        LOGGER.info("Adding steering wheel and transmission system to Create...");
     }
 }
