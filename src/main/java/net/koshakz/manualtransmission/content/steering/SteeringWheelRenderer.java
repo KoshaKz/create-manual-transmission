32d5e055af6475206cebcc5d8613905fe1dadf88
KoshaKz <viskas228228@gmail.com>
2026-02-05 12:15:31 +0100

    Fix compile error: Remove Flywheel Backend import incompatible with newer Flywheel API

--- src/main/java/net/koshakz/manualtransmission/content/steering/SteeringWheelRenderer.java
+++ src/main/java/net/koshakz/manualtransmission/content/steering/SteeringWheelRenderer.java
@@ -1,6 +1,9 @@
 package net.koshakz.manualtransmission.content.steering;
 
-import com.jozufozu.flywheel.backend.Backend;
+// import com.jozufozu.flywheel.backend.Backend; 
+// Flywheel backend import changed in newer versions or is internal.
+// For standard kinetic renderer we don't strictly need it if we call super.
+
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
@@ -14,7 +17,8 @@ public class SteeringWheelRenderer extends KineticBlockEntityRenderer<SteeringWh
 
     @Override
     protected void renderSafe(SteeringWheelBlockEntity be, float partialTicks, com.mojang.blaze3d.vertex.PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
-        if (Backend.canUseInstancing(be.getLevel())) return;
+        // if (Backend.canUseInstancing(be.getLevel())) return; // Removed for compatibility
+        
         super.renderSafe(be, partialTicks, ms, buffer, light, overlay);
     }
 }
